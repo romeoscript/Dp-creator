f7d75961f04d4dfd5353206da97000db175f7e28
romeoscript <romeobourne211@gmail.com>
2022-09-30 10:29:38 +0100

    'firstone'

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+let mainBox = document.querySelector('.viewed__dp')
+let showBox = document.querySelector('.button__create')
+let backDrop = document.querySelector('.backdrop')
 
+showBox.addEventListener('click',()=>{
+    mainBox.classList.add('mainbox')
+    backDrop.classList.add('backdropFilter')
+    
+})
+backDrop.addEventListener('click',()=>{
+    mainBox.classList.remove('mainbox')
+    backDrop.classList.remove('backdropFilter')
+    
+})
