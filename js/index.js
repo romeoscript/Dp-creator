1b3b573b3b2763af4f88714382781204cfbebbfb
romeoscript <romeobourne211@gmail.com>
2022-09-30 11:34:56 +0100

    'sidebar'

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 let mainBox = document.querySelector('.viewed__dp')
 let showBox = document.querySelector('.button__create')
 let backDrop = document.querySelector('.backdrop')
+let displayUl = document.querySelector('.displayUL')
+let mainAside = document.querySelector('.mainaside')
+let chevy = document.querySelector('.chevy')
 
 showBox.addEventListener('click',()=>{
     mainBox.classList.add('mainbox')
@@ -11,4 +14,9 @@ backDrop.addEventListener('click',()=>{
     mainBox.classList.remove('mainbox')
     backDrop.classList.remove('backdropFilter')
     
+})
+
+displayUl.addEventListener('click',()=>{
+    mainAside.classList.toggle('aside1')
+    chevy.classList.toggle('chevy1')
 })
