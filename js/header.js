014f38acb88d9d69e68ec6c581d1f3a9da85eeb0
Romeoscript <romeobourne211@gmail.com>
2022-10-06 09:56:19 +0100

    music page

--- js/header.js
+++ js/header.js
@@ -1,21 +1,22 @@
 const barBtn = document.querySelector(".bar-btn");
 //console.log(barBtn)
 //Listen to click event
-barBtn.addEventListener("click",function(e){
-	//target the nav-menu
- const navMenu = document.querySelector(".nav-menu");
+barBtn.addEventListener("click", function (e) {
+  //target the nav-menu
+  const navMenu = document.querySelector(".nav-menu");
+  const openDiv = document.querySelector(".floatin__div");
+  const openMenu = document.querySelector(".open");
+  
+  navMenu.classList.add("open");
+  e.target.classList.add("close");
+  e.target.innerHTML = "&#10006";
+  openDiv.classList.add('opendiv')
 
- const openMenu = document.querySelector(".open");
-
- navMenu.classList.add("open");
- e.target.classList.add("close");
- e.target.innerHTML = "&#10006";
-
- //close the nav-menu
- if(openMenu){
- navMenu.classList.remove("open");
- e.target.innerHTML = "&#9776";
- e.target.classList.remove("close");
- }
-
-})
+  //close the nav-menu
+  if (openMenu) {
+    navMenu.classList.remove("open");
+    e.target.innerHTML = "&#9776";
+    e.target.classList.remove("close");
+	openDiv.classList.remove('opendiv')
+  }
+});
